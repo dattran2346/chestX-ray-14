bac19532cee5076b5bc62cb04735056774d43fdb
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2019-09-04 16:24:36 +0530

    Removed the old directory

--- old/csv/link_chexnet.sh
+++
@@ -1,4 +0,0 @@
-ln -sf $(pwd)/train_list.csv ~/data/xray-thesis/chestX-ray14/train.csv
-ln -sf $(pwd)/val_list.csv ~/data/xray-thesis/chestX-ray14/val.csv
-ln -sf $(pwd)/test_list.csv ~/data/xray-thesis/chestX-ray14/test.csv
-
